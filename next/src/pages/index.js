dd9e9a242ce1d7674d48480b06734375705b04cb
Mathouzalem <bonhommemathilde@gmail.com>
2023-12-04 10:36:12 +0100

    modif index

--- next/src/pages/index.js
+++ next/src/pages/index.js
@@ -27,7 +27,7 @@ export default function Home() {
             <div className='w-1/2 flex flex-col items-center self-center lg:w-full lg:text-center'>
               <AnimatedText text="Bienvenue sur mon Portfolio !" className='!text-6xl !text-left xl:!text-5xl lg:!text-center lg:!text:6xl md:!text-5xl sm:!text:3xl'/>
               <p className='text-justify my-4 text-base font-medium md:text-sm sm:text-xs'>
-                Moi c&apos;est Mathilde, j&apos;ai 26 ans et je suis développeuse web Fullstack Ruby on Rails. Ce site est entièrement créé en ReactJS et NextJS car je les apprends !
+                Moi c&apos;est Mathilde, j&apos;ai 26 ans et je suis développeuse web Fullstack Ruby on Rails. Ce site est entièrement créé grâce à NextJS, que j'apprends !
               </p>
               <div className='flex items-center self-start mt-2 lg:self-center'>
                 <Link href="/MathildeBonhomme.pdf" target={"_blank"} className="flex items-center bg-dark text-light p-2.5 px-6 rounded-lg tect-lg font-semibold hover:bg-light hover:text-dark border border-solid border-transparent hover:border-dark dark:bg-light dark:text-dark hover:dark:bg-dark hover:dark:text-light hover:dark:border-light md:p-2 md:px-4 md:text-base"
